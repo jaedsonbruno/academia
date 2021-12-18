e895ea856d9ca475c94b43f2f7887251a9854615
Jaedson <jaedsonbruno@gmail.com>
2021-12-17 21:25:55 -0300

    Inserção de novo teste

--- src/test/java/academia/AlunoTest.java
+++ src/test/java/academia/AlunoTest.java
@@ -9,9 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AlunoTest {
     
     @Test
-    void testeNome(){
+    void testeNomeString(){
         Aluno a1 = new Aluno();
         a1.setNome("Jaedson");
         assertTrue(a1.getNome().equals("Jaedson"));
+    }
+
+    @Test
+    void testNomeDiferente(){
+        Aluno a1 = new Aluno();
+        a1.setNome("Gabriel");
+        assertFalse(a1.getNome().equals("Jaedson"));
     }
 }
