197cb0b4b8ca990601bb766c2640f4b923341037
Jaedson <jaedsonbruno@gmail.com>
2021-12-20 17:44:17 -0300

    criação da interface

---
+++ src/main/java/academia/model/Exercicio.java
@@ -0,0 +1,13 @@
+package academia.model;
+
+public interface Exercicio {
+    
+    public int getReperticoes();
+
+    public void setReperticoes(int reperticoes);
+
+    public String getNomeExercicio();
+
+    public void setNomeExercicio(String nome);
+   
+}
